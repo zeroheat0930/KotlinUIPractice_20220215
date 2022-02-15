991d8ef5f7467a0c71ce877ab7540d043ffceecc
zeroheat0930 <dongjunkorea@naver.com>
2022-02-15 14:45:19 +0900

    확인 버튼 눌리면 - inputMessage 변수 생성 - edtMessage에 적힌 문구를 string으로 바꿔서 대입 - 토스트로 변수에 저장된 문구 출력

--- app/src/main/java/com/zeroheat/kotlinuipractice_20220215/MainActivity.kt
+++ app/src/main/java/com/zeroheat/kotlinuipractice_20220215/MainActivity.kt
@@ -2,10 +2,25 @@ package com.zeroheat.kotlinuipractice_20220215
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//         버튼이 눌릴때 -> 입력된 메세지가 어떤건지 추출. -> 토스트로 띄워주기.
+
+        btnOk.setOnClickListener {
+//            버튼이 눌릴때 마다 실행
+//            -> 입력된 메세지는? 변수에 담아두자.
+
+            val inputMessage = edtMessage.text.toString() // EditText의 문구(text)를 추출 ->  String 으로 변환까지
+
+            Toast.makeText(this, inputMessage, Toast.LENGTH_SHORT).show()
+
+
+        }
     }
 }
